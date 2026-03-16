4081fa9123515a2808f9f46c6a070a56075fa4ce
Arthur Cocker <arthurcocker02@gmail.com>
2026-03-16 14:45:28 +1100

    add get and put requests

--- server.js
+++ server.js
@@ -15,6 +15,27 @@ app.post("/test", (req, res) => {
   res.json({ status: "ok", received: req.body });
 });
 
+// get request
+app.get("/test", (req, res) => {
+  console.log("Received GET:");
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
+
+  // Echo back
+  res.json({ status: "ok", received: req.body });
+});
+
+// put request
+app.put("/test", (req, res) => {
+  console.log("Received PUT:");
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
+
+  // Echo back
+  res.json({ status: "ok", received: req.body });
+});
+
+
 app.listen(port, () => {
   console.log(`Debug server listening at http://localhost:${port}`);
 });
